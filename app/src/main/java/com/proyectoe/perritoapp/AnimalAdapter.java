57a496092d90b6aedeb91d51424e9b4a9eb450ae
hugoLara <hugoLara.h@gmail.com>
2020-03-13 12:41:04 -0300

    perritoAPP implementacion de recicleview

--- app/src/main/java/com/proyectoe/perritoapp/AnimalAdapter.java
+++ app/src/main/java/com/proyectoe/perritoapp/AnimalAdapter.java
@@ -1,8 +1,54 @@
 package com.proyectoe.perritoapp;
 
-public class AnimalAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.proyectoe.perritoapp.model.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalAdapter  extends RecyclerView.Adapter<AnimalAdapter.ViewHolder> {
+
+
+    private LayoutInflater mLayoutInflater;
+    private List<Animal> animalList = new ArrayList<>(); //dataSet
+    private Context mContext;
+    private AnimalAdapter1.OnItemClickListener listener;
+
+    public AnimalAdapter(List<Animal> animalList, Context mContext, AnimalAdapter1.OnItemClickListener listener) {
+        this.animalList = animalList;
+        this.mContext = mContext;
+        this.listener = listener;
+    }
 
 
 
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+        private ImageView imageView;
+        private TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageViewListItem);
+            textView = itemView.findViewById(R.id.textVIewListItem);
+            itemView.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+
+
+        }
+    }
+
+    public interface OnItemClickListener{
+        public void onClick(AnimalAdapter.ViewHolder viewHolder, String nameAnimal, String url);
+    }
 }
